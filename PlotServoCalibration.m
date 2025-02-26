2028689e5b58c10ab086bef8670b21e9890625b9
v1v14nb <156724407+v1v14nb@users.noreply.github.com>
2025-02-26 18:25:24 -0500

    Update PlotServoCalibration.m
    added linear regression

--- PlotServoCalibration.m
+++ PlotServoCalibration.m
@@ -1,4 +1,6 @@
 % Plot continuous servo motor calibration results
+close all
+clear all
 
 % data
 servo1 = [
@@ -58,7 +60,7 @@ PWM2 = servo2(:,1);
 RPS2 = servo2(:,2) / 30;
 
 % Plot
-figure;
+figure(1);
 hold on;
 plot(PWM1, RPS1, 'bo-', 'LineWidth', 2, 'MarkerSize', 6, 'DisplayName', 'Servo 1 (shoulder)');
 plot(PWM2, RPS2, 'ro-', 'LineWidth', 2, 'MarkerSize', 6, 'DisplayName', 'Servo 2 (elbow)');
@@ -70,3 +72,47 @@ legend('Location', 'best');
 grid on;
 
 hold off;
+
+
+% Flip rates for PWM values below 1500 (clockwise rotation should be negative)
+RPS1(PWM1 < 1500) = -RPS1(PWM1 < 1500);
+RPS2(PWM2 < 1500) = -RPS2(PWM2 < 1500);
+
+% bound data to when it is changing
+PWM1_i = (PWM1 >= 1000) & (PWM1 <= 2000);
+PWM2_i = (PWM2 >= 1000) & (PWM2 <= 2000);
+
+PWM1_bounded = PWM1(PWM1_i);
+PWM2_bounded = PWM2(PWM2_i);
+
+RPS1_bounded = RPS1(PWM1_i);
+RPS2_bounded = RPS2(PWM2_i);
+
+% linear regression model
+p1 = polyfit(PWM1_bounded, RPS1_bounded, 1);
+p2 = polyfit(PWM2_bounded, RPS2_bounded, 1);
+
+% best fit lines
+pwm_range = linspace(1000, 2000, 100);
+fit1 = polyval(p1, pwm_range);
+fit2 = polyval(p2, pwm_range);
+
+figure(2)
+hold on;
+
+plot(PWM1_bounded, RPS1_bounded, 'bo', 'MarkerSize', 6, 'DisplayName', 'Servo 1 Data');
+plot(PWM2_bounded, RPS2_bounded, 'ro', 'MarkerSize', 6, 'DisplayName', 'Servo 2 Data');
+
+plot(pwm_range, fit1, 'b-', 'LineWidth', 2, 'DisplayName', sprintf('Servo 1 Fit: RPS = %.4f * PWM + %.4f', p1(1), p1(2)));
+plot(pwm_range, fit2, 'r-', 'LineWidth', 2, 'DisplayName', sprintf('Servo 2 Fit: RPS = %.4f * PWM + %.4f', p2(1), p2(2)));
+
+xlabel('PWM (microseconds)');
+ylabel('Rate (rev/s)');
+title('Continuous Servo Calibration: PWM vs Revolutions per Second');
+legend('Location', 'best');
+grid on;
+hold off;
+
+% Print best-fit equations
+fprintf('Servo 1 Best Fit: RPS = %.4f * PWM + %.4f\n', p1(1), p1(2));
+fprintf('Servo 2 Best Fit: RPS = %.4f * PWM + %.4f\n', p2(1), p2(2));
